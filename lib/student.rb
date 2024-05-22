e938d0078d263979c6499e8bf91bb3b745ef3c63
Cameron Pittman <166861602+CPPittman@users.noreply.github.com>
2024-05-22 12:33:50 -0600

    updated main files

---
+++ lib/student.rb
@@ -0,0 +1,22 @@
+require './lib/student'
+
+def initialize(student)
+  @student = student
+    puts "student"
+    
+  end
+end   
+
+student = Student.new([name: "Morgan", age: 21])
+student.name = "Morgan"
+student.age = 21
+student.scores = []
+student.log_score(89)
+student.log_score(78)
+student.scores = [89, 78]
+# student.grade = 83.5
+# add numbers in array, divide by number of items in array, 2
+def average([89, 78])
+    # @student.grade = student.grade
+    # student.grade = average
+end    
