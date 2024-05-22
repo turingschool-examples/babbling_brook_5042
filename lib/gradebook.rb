0041979e60c875b2e6ee5d8fdcd4f48435f8cc13
Tyler Noble <tmnoble11@gmail.com>
2024-05-22 10:33:00 -0600

    Added: Gradebook class with initialize method. Added: describe initialize tests.

---
+++ lib/gradebook.rb
@@ -0,0 +1,7 @@
+class Gradebook
+    attr_reader :instructor, :courses
+    def initialize(instructor)
+        @instructor = instructor
+        @courses = []
+    end
+end
--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -1,1 +1,15 @@
 require 'spec_helper'
+
+RSpec.describe Gradebook do
+    describe '#initialize' do
+        it 'creates a new gradebook' do
+            instructor = Gradebook.new("Sally Teacher")
+            expect(instructor).to be_a(Gradebook)
+        end
+
+        it 'returns instructors name' do
+            instructor = Gradebook.new("Sally Teacher")
+            expect(instructor.instructor).to eq("Sally Teacher")
+        end
+    end
+end
