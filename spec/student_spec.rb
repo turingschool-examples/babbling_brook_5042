146d7dd7bba93d5ebaf2bce600f110d70a83f2ec
Cameron Pittman <166861602+CPPittman@users.noreply.github.com>
2024-05-22 10:22:35 -0600

    student class creation

---
+++ spec/student_spec.rb
@@ -0,0 +1,10 @@
+require './lib/student'
+student = Student.new([name: "Morgan", age: 21])
+student.name = "Morgan"
+student.age = 21
+student.scores = []
+student.log_score(89)
+student.log_score(78)
+student.scores = [89, 78]
+# student.grade = 83.5
+# add numbers in array, divide by number of items in array, 2
