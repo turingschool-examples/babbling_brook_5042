9f7491ba9f9bc8ad7acda38055c7dab85076faa0
Cory Bretsch <corybretsch@gmail.com>
2024-05-22 10:24:14 -0500

    tests: Student #initialize

---
+++ spec/student_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+RSpec.describe Student do
+  describe '#initialize' do
+    it "exists" do
+      student = Student.new({name: "Morgan", age: 21})
+
+      expect(student).to be_a Student
+    end
+
+    it "has a name" do
+      student = Student.new({name: "Morgan", age: 21})
+
+      expect(student.name).to eq ("Morgan")
+    end
+
+    it "has an age" do
+      student = Student.new({name: "Morgan", age: 21})
+
+      expect(student.age).to be 21
+    end
+
+    it "can have array of scores" do
+      student = Student.new({name: "Morgan", age: 21})
+
+      expect(student.scores).to be []
+    end
+
+    
+  end
+
+
+
+
+
+end
