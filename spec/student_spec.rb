d7a67268b453a84843fc273e641ddfdfb7b5f0a2
Cameron Pittman <166861602+CPPittman@users.noreply.github.com>
2024-05-22 12:28:33 -0600

    average student.log scores

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,9 +1,10 @@
 require './lib/student'
 
 def initialize(student)
-    @student = student
+  @student = student
     puts "student"
-    end
+    
+  end
 end   
 
 student = Student.new([name: "Morgan", age: 21])
@@ -15,3 +16,7 @@ student.log_score(78)
 student.scores = [89, 78]
 # student.grade = 83.5
 # add numbers in array, divide by number of items in array, 2
+def average([89, 78])
+    # @student.grade = student.grade
+    # student.grade = average
+end    
