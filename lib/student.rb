f0642846c396265bdaca49cfda7b067145ddc17d
Karl Fallenius <smilodon.p@gmail.com>
2024-05-22 09:56:11 -0600

    addded student class and passing tests

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,17 @@
 class Student
-  attr_reader :name, :age
+  attr_reader :name, :age, :scores
 
   def initialize(student_info)
     @name = student_info[:name]
     @age = student_info[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+  
+  def grade
+    @scores.inject {|sum, next_element| sum + next_element}.to_f / @scores.size
   end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -9,5 +9,19 @@ RSpec.describe Student do
     expect(@student).to be_an_instance_of(Student)
     expect(@student.name).to eq("Morgan")
     expect(@student.age).to eq(21)
+    expect(@student.scores).to eq([])
+  end
+
+  it 'can log scores' do
+    @student.log_score(89)
+    @student.log_score(78) 
+
+    expect(@student.scores).to eq([89, 78])
+  end
+
+  it 'can calculate students grade' do
+    @student.log_score(89)
+    @student.log_score(78)
+    expect(@student.grade).to eq(83.5)
   end
 end
