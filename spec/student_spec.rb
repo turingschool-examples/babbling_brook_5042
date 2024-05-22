7b6bcadc50758826701dcef083ca6b713e5eacde
Evan Davis <evandavis427@gmail.com>
2024-05-22 09:56:02 -0600

    Fixes syntax error in test file

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,31 +1,31 @@
 require 'rspec'
 require 'student'
 
-it should be an_instance_of(Student) 
+RSpec.describe Student do
+  it 'initializes with a name and age' do
+    student = Student.new('Morgan', 21)
+    expect(student.name).to eq('Morgan')
+    expect(student.age).to eq(21)
+  end
 
-it 'initializes with a name and age' do
- student = Student.new('Morgan', 21)
- expect(student.name).to eq('Morgan')
- expect(student.age).to eq(21)
-end
+  it 'does not have any scores by default' do
+    student = Student.new('Morgan', 21)
+    expect(student.scores).to be_empty
+  end
 
-it 'does not have any scores by default' do
- student = Student.new('Morgan', 21)
- expect(student.scores).to be_empty
-end
+  context 'when log_score is called' do
+    it 'adds a score to the scores array' do
+      student = Student.new('Morgan', 21)
+      student.log_score(89)
+      student.log_score(78)
+      expect(student.scores).to include(89, 78)
+    end
 
-context 'when log_score is called' do
- it 'adds a score to the scores array' do
-   student = Student.new('Morgan', 21)
-   student.log_score(89)
-   student.log_score(78)
-   expect(student.scores).to include(89, 78)
- end
-
- it 'calculates the average grade' do
-   student = Student.new('Morgan', 21)
-   student.log_score(89)
-   student.log_score(78)
-   expect(student.grade).to eql(83.5)
- end
+    it 'calculates the average grade' do
+      student = Student.new('Morgan', 21)
+      student.log_score(89)
+      student.log_score(78)
+      expect(student.grade).to eql(83.5)
+    end
+  end
 end
