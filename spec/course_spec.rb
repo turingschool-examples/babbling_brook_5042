353f567b1623e02ff3e611b789f85cd71f542a55
Cory Bretsch <corybretsch@gmail.com>
2024-05-22 10:47:30 -0500

    tests: Course #enroll

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -25,17 +25,36 @@ RSpec.describe Course do
 
       expect(course.students).to eq []
     end
-
-
-
   end
 
+  describe "#enroll" do
+    it "can have availability" do
+      course = Course.new("Calculus", 2) 
 
+      expect(course.full?).to be false
+    end
 
+    it "can enroll a student" do
+      course = Course.new("Calculus", 2)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})  
 
+      course.enroll(student1)   
+      course.enroll(student2)   
 
+      expect(course.students).to eq [student1, student2]
+    end
 
+    it "can be closed" do
+      course = Course.new("Calculus", 2)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})  
 
+      course.enroll(student1)   
+      course.enroll(student2)   
 
-
+      expect(course.full?).to be true
+    end
+  end
+  
 end
