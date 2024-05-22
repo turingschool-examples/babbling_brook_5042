7576390d2c7cac558d0197eeea0a2fb59b46942e
Evan Davis <evandavis427@gmail.com>
2024-05-22 09:44:54 -0600

    Add tests to the student_spec.rb file

---
+++ spec/student_spec.rb
@@ -0,0 +1,31 @@
+require 'rspec'
+require 'student'
+
+it should be an_instance_of(Student) 
+
+it 'initializes with a name and age' do
+ student = Student.new('Morgan', 21)
+ expect(student.name).to eq('Morgan')
+ expect(student.age).to eq(21)
+end
+
+it 'does not have any scores by default' do
+ student = Student.new('Morgan', 21)
+ expect(student.scores).to be_empty
+end
+
+context 'when log_score is called' do
+ it 'adds a score to the scores array' do
+   student = Student.new('Morgan', 21)
+   student.log_score(89)
+   student.log_score(78)
+   expect(student.scores).to include(89, 78)
+ end
+
+ it 'calculates the average grade' do
+   student = Student.new('Morgan', 21)
+   student.log_score(89)
+   student.log_score(78)
+   expect(student.grade).to eql(83.5)
+ end
+end
