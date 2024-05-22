477a34aec923db813e8e559025297803308f3454
Cory Bretsch <corybretsch@gmail.com>
2024-05-22 11:15:33 -0500

    tests: CourseBook #add_course

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -21,6 +21,19 @@ RSpec.describe GradeBook do
     end
   end
 
+  describe "#add_course" do
+    it "can add a course" do
+      gradebook = GradeBook.new("Mr. Scrynt")
+      course1 = Course.new("Calculus", 2)
+      course2 = Course.new("Physics", 4)
+
+      gradebook.add_course(course1)
+      gradebook.add_course(course2)
+
+      expect(gradebook.courses).to eq [course1, course2]
+    end
+  end
+
   
 
 end
