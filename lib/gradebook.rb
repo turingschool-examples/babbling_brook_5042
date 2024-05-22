6f0ddb1fe7a0e15525b6a359dd0d738d65ca40a3
Cory Bretsch <corybretsch@gmail.com>
2024-05-22 11:54:59 -0500

    test and feat: Course #list_all_students

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -9,4 +9,12 @@ class GradeBook
   def add_course(course)
     @courses << course
   end
+
+  def list_all_students
+    course_student_hash = {}
+    @courses.each do |course|
+      course_student_hash[course] = course.students
+    end
+    course_student_hash
+  end
 end
--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -34,6 +34,42 @@ RSpec.describe GradeBook do
     end
   end
 
+  describe "#list_all_students" do
+    it "can list students in a course" do
+      gradebook = GradeBook.new("Mr. Scrynt")
+      course1 = Course.new("Calculus", 2)
+      course2 = Course.new("Physics", 4)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})  
+
+      course1.enroll(student1)
+      course1.enroll(student2)
+      gradebook.add_course(course1)
+
+      expect(gradebook.list_all_students).to eq ({course1 => [student1, student2]})
+    end
+
+    it "can list all students in all courses" do
+      gradebook = GradeBook.new("Mr. Scrynt")
+      course1 = Course.new("Calculus", 2)
+      course2 = Course.new("Physics", 4)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})  
+
+      course1.enroll(student1)
+      course1.enroll(student2)
+      course2.enroll(student1)
+      course2.enroll(student2)
+      gradebook.add_course(course1)
+      gradebook.add_course(course2)
+
+      expect(gradebook.list_all_students).to eq ({
+                                                  course1 => [student1, student2], 
+                                                  course2 => [student1, student2]
+                                                }) 
+    end
+  end
+
   
 
 end
