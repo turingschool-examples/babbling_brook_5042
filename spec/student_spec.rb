48327e1514f8ef210cce9e8225a5ab9bdb22dbb3
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-05-22 09:31:30 -0600

    Create tests for Student class

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,27 +1,64 @@
-
-
+require 'spec_helper'
 
 # pry(main)> require './lib/student'
 # # => true
+RSpec.describe Student do
+  before(:each) do
 
-# pry(main)> student = Student.new({name: "Morgan", age: 21})    
-# # => #<Student:0x00007fe196b0c050...>
+    @student = Student.new({name: "Morgan", age: 21})
 
-# pry(main)> student.name
-# # => "Morgan"
+  end
+  it "is an instance of the Student class" do
+    expect(@student).to be_an_instance_of(Student)
+    
+    # pry(main)> student = Student.new({name: "Morgan", age: 21})    
+    # # => #<Student:0x00007fe196b0c050...>
+  end
+  
+  it "has a name" do
+    expect(@student.name).to eq("Morgan")
+    
+    # pry(main)> student.name
+    # # => "Morgan"
+  end
 
-# pry(main)> student.age
-# # => 21
+  it "has an age" do
+    expect(@student.age).to eq(21)
 
-# pry(main)> student.scores
-# # => []
+    # pry(main)> student.age
+    # # => 21
+  end
+  
+  it "has a score" do
+    expect(@student.scores).to eq([])
 
-# pry(main)> student.log_score(89)
+    # pry(main)> student.scores
+    # # => []
+  end
 
-# pry(main)> student.log_score(78)    
+  it "can log a student's scores" do
+    @student.log_score(89)
+    # pry(main)> student.log_score(89)
+    @student.log_score(78)
+    # pry(main)> student.log_score(78)    
 
-# pry(main)> student.scores
-# # => [89, 78]
+    expect(@student.scores).to eq([89, 78])
+    # pry(main)> student.scores
+    # # => [89, 78]
+  end
 
-# pry(main)> student.grade # Average of all the scores
-# # => 83.5
+  it "can average student scores" do
+    expect(@student.grade).to eq(83.5)
+    
+    # pry(main)> student.grade # Average of all the scores
+    # # => 83.5
+  end
+end
+
+
+
+
+
+
+
+
