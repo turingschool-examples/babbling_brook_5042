95f79c6f3c973676a6c2261c2d4d85c389ab0701
Cameron Pittman <166861602+CPPittman@users.noreply.github.com>
2024-05-22 11:56:17 -0600

    adding breaks

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -1,10 +1,12 @@
 require './lib/course'
 require './lib/student'
+
 course = Course.new("Calculus", 2)
 course.name = "Calculus"
 course.capacity = 2
 course.students = []
 course.full? = false
+
 student1 = Student.new({name: "Morgan", age: 21})
 student2 = Student.new({name: "Jordan", age: 29})
 course.enroll(student1)
